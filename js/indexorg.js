ef77cb6d38501d1a80921fe2fbe22281e92a71ab
nannany <ymym1990ymym@gmail.com>
2020-01-25 21:19:43 +0900

    ちょっと手直し

--- js/indexorg.js
+++ js/indexorg.js
@@ -1,5 +1,4 @@
 window.addEventListener('DOMContentLoaded', init);
-
 function init() {
     // レンダラーを作成
     const renderer = new THREE.WebGLRenderer({
@@ -11,7 +10,7 @@ function init() {
     renderer.setPixelRatio(1);
     renderer.setSize(width, height);
     console.log(window.devicePixelRatio);
-    console.log(width + ", " + height);
+    console.log(width+", "+height);
 
     // シーンを作成
     const scene = new THREE.Scene();
@@ -21,6 +20,7 @@ function init() {
     camera.position.set(0, 400, -1000);
 
     const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    //camera.lookAt(new THREE.Vector3(0, 400, 0));
 
     // Load GLTF or GLB
     const loader = new THREE.GLTFLoader();
@@ -29,18 +29,19 @@ function init() {
     let model = null;
     loader.load(
         url,
-        function (gltf) {
+        function ( gltf ){
             model = gltf.scene;
-            // model.name = "model_with_cloth";
+            model.name = "model_with_cloth";
             model.scale.set(400.0, 400.0, 400.0);
-            model.position.set(0, -400, 0);
-            scene.add(gltf.scene);
+            model.position.set(0,-400,0);
+            scene.add( gltf.scene );
 
-            // model["test"] = 100;
+            model["test"] = 100;
+            console.log("model");
         },
-        function (error) {
-            console.log('An error happened');
-            console.log(error);
+        function ( error ) {
+            console.log( 'An error happened' );
+            console.log( error );
         }
     );
     renderer.gammaOutput = true;
@@ -56,14 +57,38 @@ function init() {
 
     // 初回実行
     tick();
-
     function tick() {
         controls.update();
 
-        if (model != null) {
+        scene.traverse(function(obj) {
+            if(obj.name == "J_Bip_C_Chest"){
+                obj.rotation.z += 2 /180*3.1415;
+            }
+        });
+        if (model != null){
             console.log(model);
         }
         renderer.render(scene, camera);
         requestAnimationFrame(tick);
+    }
+
+
+    // 初期化のために実行
+    onResize();
+    // リサイズイベント発生時に実行
+    window.addEventListener('resize', onResize);
+    function onResize() {
+        // サイズを取得
+        width = document.getElementById('main_canvas').getBoundingClientRect().width;
+        height = document.getElementById('main_canvas').getBoundingClientRect().height;
+
+        // レンダラーのサイズを調整する
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(width, height);
+
+        // カメラのアスペクト比を正す
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        console.log(width);
     }
 }
